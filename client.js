13faa4dfdebc90b6411be04e76ac1677a1626c1e
Robyn Noble <robynhurd09@gmail.com>
2021-04-09 19:28:42 -0500

    delete function deleting from DOM, monthly total started but not working yet

--- client.js
+++ client.js
@@ -1,42 +1,57 @@
+console.log('Hi jQuery');
+
+const startingEmployee = [ ];
+
 $(document).ready(onReady);
-console.log('Hi jQuery')
 
 function onReady() {
     //setup handlers
     $('#submit-employee').on('click', handleAddEmployee);
-}
-
-function handleAddEmployee() {
-    console.log('Adding an employee ');
-    let newEmployee = {
-        firstName: $('#in-first-name').val(),
-        lastName: $('#in-last-name').val(),
-        employeeID:$('#in-employee-id').val(),
-        jobTitle:$('#in-job-title').val(),
-        annualSalary:$('#in-annual-salary').val(),
-    };
-    console.log(newEmployee);
+    $('#out-employees').on('click', '.delete', deleteEmployee);
     renderEmployees();
-    clearInputParams();   
-}
+}//end onReady
+function deleteEmployee() {
+    console.log('Deleting employee...');
+    $(this).closest('tr').remove();
+}//end deleteEmployee
 
 function renderEmployees() {
     //clear inputs
     $('#out-employees').empty();
-
-    //Loop through objects & add row to the table on the DOM
-    for( let person of newEmployee ){
+    //Loop array and add table row to DOM
+    for( let person of startingEmployee){
         $('#out-employees').append(`
         <tr>
             <td>${person.firstName}</td>
             <td>${person.lastName}</td>
             <td>${person.employeeID}</td>
             <td>${person.jobTitle}</td>
-            <td>${person.annualSalary}</td>
+            <td>${'$'+ person.annualSalary}</td>
+            <td>
+                <button class="delete">Delete</button>
+            </td>
         </tr>
         `)
     }
-}
+}//end renderEmployees
+
+function handleAddEmployee() {
+    console.log('Adding an employee: ');
+        //Get inputs from browser
+        let newEmployee = {
+        firstName: $('#in-first-name').val(),
+        lastName: $('#in-last-name').val(),
+        employeeID: $('#in-employee-id').val(),
+        jobTitle: $('#in-job-title').val(),
+        annualSalary: $('#in-annual-salary').val(),
+        };
+        //Add employee to array
+    startingEmployee.push(newEmployee);
+    console.log(startingEmployee)
+    renderEmployees();
+    clearInputParams();  
+}//end handleAddEmployee
+
 
 function clearInputParams() {
     $('#in-first-name').val('');
@@ -44,4 +59,14 @@ function clearInputParams() {
     $('#in-employee-id').val('');
     $('#in-job-title').val('');
     $('#in-annual-salary').val('');
-}
+}//end clearInputParams
+
+function monthlySalaryTotal() {
+    let sum = 0;
+    for(let x=0; x<startingEmployee[x].length; x++){
+        sum += startingEmployee[x].annualSalary / 12;
+    }
+    return sum;
+}//end monthlySalaryTotal
+console.log('The monthly total salary is:', monthlySalaryTotal(startingEmployee));
+
